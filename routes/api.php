306c23ec5c7474274931a702aa1f8ae9ab8ad193
黄海林 <10000@cxzfb.cn>
2017-05-25 15:35:46 +0800

    删除了不必要的文件，修复了不显示封面图的Bug

--- routes/api.php
+++ routes/api.php
@@ -16,24 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::group(['middleware'=>'api.base'], function(){
-    Route::post('/regist', 'api\AuthController@regist'); // 注册
-    Route::post('/access_token', 'api\AuthController@login'); // 获取登录凭证
-    Route::post('/refresh_token', 'api\AuthController@refreshToken'); // 刷新令牌，需要Token
-
-    Route::post('/account/bind', 'api\AuthController@bindAccount'); // 根据用户信息绑定第三方
-    Route::post('/account/regist', 'api\AuthController@registAccount'); // 根据第三方信息注册新账号
-    Route::post('/common/sms/regist', 'api\CommonController@registSMS'); // 注册的短信
-    
-    
-    Route::get('/post', 'api\PostController@post_list');
-    Route::get('/post/classify', 'api\PostController@post_classify_list');
-    Route::get('/post/content', 'api\PostController@post_content');
-    Route::get('/classify', 'api\PostClassifyController@classify_list');
-
-
-    Route::group(['middleware'=>'api.permission'], function(){
-
-    });
-});
